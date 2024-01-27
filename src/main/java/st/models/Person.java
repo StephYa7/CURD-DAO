eb52e0fcca6245cf0b8aac67e71a39983d7f091a
StephYa7 <1000p@mail.ru>
2024-01-27 20:18:29 +0700

    added validator for all fields in create and update method

--- src/main/java/st/models/Person.java
+++ src/main/java/st/models/Person.java
@@ -2,17 +2,28 @@ package st.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Person {
+
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
+
+    @Min(value = 0, message = "Age should not be negative")
     private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
     private String email;
-
-
-
-    public Person() {
-    }
 }
